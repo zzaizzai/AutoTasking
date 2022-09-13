76740698a3ffcfdbf6440d5a102fbdbb8b0a7d7d
zzaizzai <junsai0530@gmail.com>
2022-09-14 01:32:04 +0900

    init

--- FindValues.py
+++ FindValues.py
@@ -7,9 +7,15 @@ wb = openpyxl.load_workbook(filePath)
 sheet = wb.worksheets[0]
 print(sheet)
 
+targetCell: str = 'aa30'
+
 for row in sheet.rows:
     for cell in row:
         if cell.value != None:
-            print(f"{cell} : {cell.value}")
+            if cell.value == targetCell:
+                print('it is target')
+                print(f'{cell.value}: {sheet.cell(row= cell.row , column= cell.column + 1).value}')
+            # print(f"{cell.coordinate}: {cell.value}")
+
 
 wb.close()
