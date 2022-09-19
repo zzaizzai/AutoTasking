2e198d95fb441143c17618f152e909217e6050f2
Junsai <69841333+zzaizzai@users.noreply.github.com>
2022-09-19 15:06:20 +0900

    Update Rheometer.py

--- Rheometer.py
+++ Rheometer.py
@@ -11,6 +11,7 @@ class Rheometer:
         self.target = target
         self.folderPath = DesktopPath + fr'\{target} Data'
         self.filePath = DesktopPath + fr'\{target} Data\レオメータ*{target}*.xls'
+        self.number_of_target: int = 0
 
     # translate from xls to xlsx
     def MakeXlsmFile(self, file: str):
@@ -27,6 +28,7 @@ class Rheometer:
         # ws = wb['Sheet1']
         ws = wb.worksheets[0]
 
+
         standard_cell = ws.cell(row=1, column=1)
         # Find 'Time' cell
         for row in ws.rows:
@@ -34,6 +36,9 @@ class Rheometer:
                 if cell.value == 'Time(NO.1)':
                     standard_cell = cell
                     break
+        
+        self.number_of_target = int(ws.cell(row=standard_cell.row - 2, column=standard_cell.column).value)
+        print(f'number of target: {self.number_of_target}')
 
         print(f'standard postion cell: {standard_cell}')
         # 1/4 data
@@ -56,14 +61,14 @@ class Rheometer:
 
         # add series
         print('adding serials')
-        for i in range(8):
+        for i in range(self.number_of_target):
             serial_name = ws.cell(row=21, column=2 + 4*i).value
             if serial_name != None:
                 print(serial_name)
                 data = openpyxl.chart.Reference(
-                    ws, min_col=2 + i*4, min_row=standard_cell.row, max_row=550)
+                    ws, min_col=2 + i*4, min_row=standard_cell.row, max_row=1000)
                 times = openpyxl.chart.Reference(
-                    ws, min_col=1, min_row=standard_cell.row + 1, max_row=550)
+                    ws, min_col=1, min_row=standard_cell.row + 1, max_row=1000)
 
                 series = openpyxl.chart.Series(
                     data, times, title_from_data=True)
@@ -114,21 +119,27 @@ class Rheometer:
         init_row = 1
         init_col = 2
 
-
-
         xlsxFilePath_copy = os.path.dirname(
             xlsxFilePath) + f'\{self.target} Data.xlsx'
         wb_copy = openpyxl.load_workbook(xlsxFilePath_copy)
         ws_copy = wb_copy.worksheets[0]
 
+        # targets
+        for i in range(self.number_of_target):
+            # print(f'{self.target}({i}) making')
+            ws_copy.cell(row=1, column=5 + i,  value=f'{self.target}({i+1})')
+
         ws_copy.cell(row=init_row, column=init_col, value='Experiemnts')
         ws_copy.cell(row=init_row, column=init_col + 1, value='Method')
         ws_copy.cell(row=init_row, column=init_col + 2, value='Unit')
 
-        ws_copy.cell(row=init_row + 1, column=init_col + 1, value='MA')
-        ws_copy.cell(row=init_row + 2, column=init_col + 1, value='MB')
-        ws_copy.cell(row=init_row + 3, column=init_col + 1, value='MC')
-
+        method = ['MH', 'ML', 't10', 't50', 't90', 'CR']
+        for i in range(len(method)):
+             ws_copy.cell(row=init_row + 1 + i, column=init_col + 1, value=method[i])
+        # ws_copy.cell(row=init_row + 1, column=init_col + 1, value='MH')
+        # ws_copy.cell(row=init_row + 2, column=init_col + 1, value='ML')
+        # ws_copy.cell(row=init_row + 3, column=init_col + 1, value='t10')
+        unit = ['kgf・cm', 'kgf・cm', 'min', 'min', 'min']
         ws_copy.cell(row=init_row + 1, column=init_col + 2, value='%')
         ws_copy.cell(row=init_row + 2, column=init_col + 2, value='%')
         ws_copy.cell(row=init_row + 3, column=init_col + 2, value='%')
@@ -157,36 +168,36 @@ class Rheometer:
 
         for i in range(5):
             ws_copy.cell(
-                row=2, column=5 + i, value=values_original(cell_init.row + 1 + i*2, cell_init.column))
+                row=2, column=5 + i, value=values_original(cell_init.row + 3 + i*2, cell_init.column))
             ws_copy.cell(
-                row=3, column=5 + i, value=values_original(cell_init.row + 1 + i*2, cell_init.column + 2))
+                row=3, column=5 + i, value=values_original(cell_init.row + 3 + i*2, cell_init.column + 1))
             ws_copy.cell(
-                row=4, column=5 + i, value=values_original(cell_init.row + 1 + i*2, cell_init.column + 4))
+                row=4, column=5 + i, value=values_original(cell_init.row + 3 + i*2, cell_init.column + 3))
 
         wb_copy.save(xlsxFilePath_copy)
         print('saved copy values of rheometer')
 
-    def removeNanCol(self):
-        wb = openpyxl.Workbook()
-        ws = wb.active
+    # def removeNanCol(self):
+    #     wb = openpyxl.Workbook()
+    #     ws = wb.active
 
-        # delete nan value col
-        excelPath = self.folderPath + fr'\{self.target} Data.xlsx'
-        df = pd.read_excel(excelPath, header=None, index_col=0)
-        df.reset_index(inplace= True, drop= True)
-        # df.loc[len(df)] = data_input
-        print(df)
-        print(len(df.columns))
+    #     # delete nan value col
+    #     excelPath = self.folderPath + fr'\{self.target} Data.xlsx'
+    #     df = pd.read_excel(excelPath, header=None, index_col=0)
+    #     df.reset_index(inplace= True, drop= True)
+    #     # df.loc[len(df)] = data_input
+    #     print(df)
+    #     print(len(df.columns))
 
-        for i in range(4, len(df.columns) + 1):
-            print(df[i])
-            # np.isnan(df[i][2])
-            if pd.isna(df[i][2]):
-            # if np.isnan(df[i][2]):
-                del df[i]
+    #     for i in range(4, len(df.columns) + 1):
+    #         print(df[i])
+    #         # np.isnan(df[i][2])
+    #         if pd.isna(df[i][2]):
+    #         # if np.isnan(df[i][2]):
+    #             del df[i]
 
-        df.to_excel(excelPath, index=False, header=False, startcol=1)
-        # wb.save(self.folderPath + fr'\{self.target} Data.xlsx')
+    #     df.to_excel(excelPath, index=False, header=False, startcol=1)
+    #     # wb.save(self.folderPath + fr'\{self.target} Data.xlsx')
 
 
 def Rheo(target: str, DesktopPath:str):
@@ -199,7 +210,7 @@ def Rheo(target: str, DesktopPath:str):
         print(file_list)
         rheo = Rheometer(target, DesktopPath)
         rheo.Rheometer()
-        rheo.removeNanCol()
+        # rheo.removeNanCol()
     else:
         print('no rheometer Data')
 
