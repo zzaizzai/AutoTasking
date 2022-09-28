523a192f00816629af0b3eda15cabb8e69164e4d
zzaizzai <junsai0530@gmail.com>
2022-09-28 22:46:19 +0900

    dd

--- HeatResist.py
+++ HeatResist.py
@@ -11,7 +11,7 @@ class HeatResist:
         self.file_dir = self.DesktopPath  + rf'\{target} Data'
         self.target = target
 
-        self.file_path = self.file_dir + rf'\{self.exp_name}*{target}*.xlsm'
+        self.file_path = self.file_dir + rf'\{self.exp_name}*{target}*.xls*'
 
         self.file_now = ''
     
@@ -39,7 +39,12 @@ class HeatResist:
         print(self.file_now)
         sheet_list = pd.ExcelFile(self.file_now).sheet_names
         print(sheet_list)
-        sheet_list.remove('設定シート')
+
+        if '設定シート' in sheet_list:
+            sheet_list.remove('設定シート')
+        else:
+            pass
+
         print(sheet_list)
 
 
@@ -59,12 +64,60 @@ class HeatResist:
 
             
         print(sheet)
-        df = pd.read_excel(self.file_now, sheet_name=sheet, header=10, index_col=1)
+        df = pd.read_excel(self.file_now, sheet_name=sheet, header=None, index_col=1)
         df = df.transpose()
         print(df.index)
 
 
         print(df)
+
+
+
+        ## title
+        print(df)
+        print(df.iloc[:,[2,3]])
+
+        print(df.loc[[4]].values.tolist()[0])
+        col_index = []
+        for i, value in enumerate(df.loc[[4]].values.tolist()[0]):
+            print(i, value)
+            if not 'nan' in str(value):
+                print('not nan')
+                col_index.append(i)
+
+        print(col_index)
+        print(df.iloc[:,col_index])
+
+        df = df.iloc[:,col_index]
+
+        title_index =  df.columns.values.tolist()
+        for i, value in enumerate(title_index):
+            if 'nan' in str(value):
+                print('nan',i)
+                title_index[i] = title_index[i-1]
+        print(title_index)
+        df.columns = title_index
+        print(df)
+
+
+
+        
+        mean_col_index = [0]
+        row_mean_str = df.loc[[3]].values.tolist()[0]
+        print(row_mean_str)
+        for i, value in enumerate(row_mean_str):
+            print(value)
+            if '中央値' in str(value):
+                print('mean str', i)
+                mean_col_index.append(i)
+
+        print(mean_col_index)
+
+        df_input = df.iloc[:,mean_col_index]
+        df_input = df_input.dropna()
+        print(df_input)
+
+        print(df_input.query("配合番号 in ['引っ張り','伸び']"))
 
         return df
 
@@ -74,7 +127,7 @@ def DoIt(target:str):
     heat.FindFile()
 
 if __name__ == '__main__':
-    target = 'FJX001'
+    target = 'CBA001'
     # target = input('target: ')
     
     DoIt(target)
