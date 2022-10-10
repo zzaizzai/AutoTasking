16b6b25d350c8fb5d76567b04778595a137f2116
zzaizzai <junsai0530@gmail.com>
2022-10-10 23:26:15 +0900

    q

---
+++ Deruta.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import Service
+
+class Deruta:
+
+    def __init__(self, target):
+        self.exp_name = '熱老化_自動集積 '
+        
+        self.target = target
+        self.file_path = Service.data_dir(target) + rf'\{self.exp_name}*{target}*.xls*'
