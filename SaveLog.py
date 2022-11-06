76b3fdf84bea3a52b7d84dd9ec77eb3a0bdf61c1
zzaizzai <junsai0530@gmail.com>
2022-11-06 18:56:02 +0900

    dd

--- SaveLog.py
+++ SaveLog.py
@@ -1,28 +1,34 @@
-import json
-import datetime
+import csv
 import os
+import datetime
 
 
-def Save_Data(name:str, target: str):
-    data = {
-        'name': name,
-        'target': target,
-        'date': str(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')),
-         }
-         
-    log_json = []
-    if os.path.exists('./log.json'):
-        json_file = open('./log.json', 'r')
-        log_json = json.load(json_file)
-        json_file.close()
+def Save_Data(name: str, target: str):
+    if os.path.isfile('./log.csv'):
+        pass
+    else:
+        print('new log file')
+        f_new = open('./log.csv', 'w')
+        f_new.write('id, name, target, time')
+        f_new.close()
 
-    log_json.append(data)
-    json_write = open('./log.json', 'w')
-    json.dump(log_json, json_write, indent=4,  separators=(',',': '))
+    f = open('./log.csv', 'r')
+    reader = csv.DictReader(f)
+    
+    count_oldest = 0
+    for row in reader:
+        if int(row["id"]) >= count_oldest:
+            count_oldest = int(row["id"])
+    f.close()
 
-    json_write.close()
+    f = open('./log.csv', 'a')
+    count_new = count_oldest + 1
+    new_data = f"\n{count_new}, {name}, {target}, {datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}"
+    f.write(new_data)
+    f.close()
 
-if __name__ == '__main__':
-    user_name = input('input user name:')
-    target = input('target: ')
-    Save_Data(str(user_name), str(target))
+
+if __name__ == "__main__":
+    user_name = input("input user name: ")
+    target = input("target: ")
+    Save_Data(user_name, target)
